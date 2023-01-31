795cb011350d7edb50dbe6d1fae029144c996283
Riley Wood <Riley@Daids-MacBook-Air.local>
2023-01-31 10:51:29 -0600

    circle-js exercises compelted

--- js/circle.js
+++ js/circle.js
@@ -4,21 +4,16 @@
     // create a circle object
     var circle = {
         radius: 3,
-
         getArea: function () {
-            // TODO: complete this method
-            // hint: area = pi * radius^2
-
-            return Math; // TODO: return the proper value
+            var area = Math.PI*Math.pow(circle.radius,2)
+            return area;
         },
-
         logInfo: function (doRounding) {
-            // TODO: complete this method.
-
-            // If doRounding is true, round the result to the nearest integer.
-            // Otherwise, output the complete value
-
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            if (doRounding===true) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: " + Math.round(circle.getArea()));
+            } else if(doRounding===false) {
+                console.log("Area of a circle with radius: " + this.radius + ", is: "+circle.getArea());
+            }
         }
     };
 
@@ -29,9 +24,7 @@
     circle.logInfo(true);
 
     console.log("=======================================================");
-    // TODO: Change the radius of the circle to 5.
-
-    // log info about the circle
+    circle.radius+=2
     console.log("Raw circle information");
     circle.logInfo(false);
     console.log("Circle information rounded to the nearest whole number");
