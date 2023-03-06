446cb6b62e2542d18ba3e969a117a6720ad3e7ca
RileyWood9984 <rjw9984@gmail.com>
2023-03-06 12:32:09 -0600

    class selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,6 +1,6 @@
 "use strict";
 $(function () {
-    alert('The DOM has Loaded');
     let contents = $('#list-item-1').html();
     alert(contents)
+    $('.codeup').css('border', '1px solid red');
 })
