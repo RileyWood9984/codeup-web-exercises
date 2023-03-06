64738db1d82269201d4347bc5348aa983bbaa76c
RileyWood9984 <rjw9984@gmail.com>
2023-03-06 15:06:50 -0600

    A bit more than requested, mouse events done

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,7 +1,32 @@
 "use strict";
 $(function () {
-    let contents = $('#list-item-1').html();
-    alert(contents)
-    $('li').css('font-size', '20px');
-    $('h1, p,li').css('background','yellow')
+    $('h1').click(
+        function () {
+            $(this).css('background-color', function () {
+                let r = Math.floor(Math.random() * 255)
+                let g = Math.floor(Math.random() * 255)
+                let b = Math.floor(Math.random() * 255)
+                let rgb = r.toString() + ', ' + g.toString() + ', ' + b.toString()
+                return 'rgb(' + rgb + ')'
+            })
+        }
+    )
+    $('h1').hover(
+        function () {
+            $(this).html('Just keep clicking!')
+        }, function () {
+            $(this).html('I am the h1').css('background-color', 'white')
+        }
+    )
+    $('p').dblclick(
+        function (){
+            $(this).css('font-size','18px')
+        }
+    )
+    $('li').hover(
+        function (){
+            $(this).css('color','red')
+        },function (){
+            $(this).css('color','black')
+        })
 })
