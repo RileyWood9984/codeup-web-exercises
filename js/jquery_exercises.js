57045120386625f627581eef4a25ff85d46a0fee
RileyWood9984 <rjw9984@gmail.com>
2023-03-06 12:38:27 -0600

    multi selectors

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -2,5 +2,6 @@
 $(function () {
     let contents = $('#list-item-1').html();
     alert(contents)
-    $('.codeup').css('border', '1px solid red');
+    $('li').css('font-size', '20px');
+    $('h1, p,li').css('background','yellow')
 })
